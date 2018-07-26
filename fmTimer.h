1de556df8a4bfb67a8ac5c9896dfc306fd39f656
SYkehaoran <18510666543@163.com>
2018-07-26 13:12:30 +0800

    “”

--- fmTimer.h
+++ fmTimer.h
@@ -1,6 +1,8 @@
 //
 //  fmTimer.h
-//
+//  NSTimer drop-in alternative that doesn't retain the target
+
+//  This class was to have a type of timer that objects could own and retain, without this creating a retain cycle ( like NSTimer causes, since it retains its target ). This way you can just release the timer in the -dealloc method of the object class that owns the timer.
 //  Created by 柯浩然 on 7/24/18.
 //
 
